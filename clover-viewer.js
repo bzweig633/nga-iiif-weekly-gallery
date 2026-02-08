3e08f97c4d42aec88d7d61f01c12f4d61c487607
Benjamin Zweig <113474064+bzweig633@users.noreply.github.com>
2026-02-08 12:26:34 -0500

    Update clover-viewer.js

--- clover-viewer.js
+++ clover-viewer.js
@@ -12,16 +12,13 @@
         const viewer = document.createElement('clover-viewer');
 
         // Configure options: Set information panel to closed
-        // In the web component, this is handled via the 'options' attribute as a JSON string
-        const options = {
+        const options = JSON.stringify({
             informationPanel: {
-                open: false,
+                open: false
             }
-        };
+        });
 
         viewer.setAttribute('options', options);
-        
-        // Set the Manifest URL
         viewer.setAttribute('iiif-content', 'https://bzweig633.github.io/nga-iiif-weekly-gallery/nga_random_collection.json');
 
         // Inject into the DOM
